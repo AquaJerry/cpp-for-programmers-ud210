c63b24cec5c604b307c2ad5d8ec7db1fa9b8c5e4
aquajerry <huopopo_zeyang@163.com>
2018-10-28 19:33:49 +0800

    feat: header file

---
+++ lesson-01-basics/main.cpp
@@ -0,0 +1,12 @@
+/* Practice using header files.
+ *
+ * Put in the header file (.hpp files) everything related to "how to do a
+ * task". Put "what to do" here.
+ */
+
+#include "main.hpp"
+
+int main()
+{
+	cout << "Hello, I use header files!";
+}
---
+++ lesson-01-basics/main.hpp
@@ -0,0 +1,9 @@
+/* This is a header file.
+ *
+ * Put everything related to "how to do a task" here. Put "what to do" in
+ * programs (.cpp files).
+ */
+
+#include <iostream>
+
+using namespace std;
