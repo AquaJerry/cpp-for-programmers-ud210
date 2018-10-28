39575db7dab93ca471d9f59d8d97993ccc7a0585
aquajerry <huopopo_zeyang@163.com>
2018-10-28 16:54:20 +0800

    fix: error: cannot assign to variable 'weight_goal' with const-qualified type 'const int'

--- lesson-01-basics/const.cpp
+++ lesson-01-basics/const.cpp
@@ -8,6 +8,6 @@ int main()
 	const int weight_goal = 100;
 
 	cout << "WeightGoal = " << weight_goal << "\n";
-	weight_goal = 200;
-	cout << "WeightGoal = " << weight_goal << "\n";
+	// weight_goal = 200;
+	// cout << "WeightGoal = " << weight_goal << "\n";
 }
