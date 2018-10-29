79cd1e9f18d59bf622d325c7729611e855fd2754
aquajerry <huopopo_zeyang@163.com>
2018-10-29 12:20:06 +0800

    update: input strings with std::cin

--- lesson-01-basics/main.cpp
+++ lesson-01-basics/main.cpp
@@ -1,12 +1,24 @@
-/* Example for std::getline() */
+/* Practice std::cin for strings.
+ *
+ * Write a program that prompts two users for their name, address, and phone
+ * number. Print the information to the console in the following format.
+ *
+ * name\n\t\taddress\n\t\tphone number
+ */
 
 #include "main.hpp"
 
+#define READ getline(cin, str)
+#define END_AT READ; cout << str <<
+
 int main()
 {
-	string nickname{};
+	string str;
 
-	cout << "Tell me your nickname? ";
-	getline(cin, nickname);
-	cout << "Hello " << nickname << endl;
+	while (READ)
+	{
+		cout << str << "\n\t\t";
+		END_AT "\n\t\t";
+		END_AT endl;
+	}
 }
