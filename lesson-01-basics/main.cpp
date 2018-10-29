27aac1fd086a6ae8a88275bfdc1b1838048271ae
aquajerry <huopopo_zeyang@163.com>
2018-10-29 11:42:29 +0800

    feat: std::getline()

--- lesson-01-basics/main.cpp
+++ lesson-01-basics/main.cpp
@@ -1,19 +1,12 @@
-/* Accept inputs from input.txt. */
+/* Example for std::getline() */
 
 #include "main.hpp"
 
 int main()
 {
-	int age{}, year{};
-	string name{};
+	string nickname{};
 
-	cout << "What year is your favorite? ";
-	cin >> year;
-	cout << "How interesting, your favorite year is " << year << "!\n";
-	cout << "At what age did you learn to ride a bike? ";
-	cin >> age;
-	cout << "How interesting you learned to ride at " << age << "!\n";
-	cout << "What's your name? ";
-	cin >> name;
-	cout << "Hello " << name << "!\n";
+	cout << "Tell me your nickname? ";
+	getline(cin, nickname);
+	cout << "Hello " << nickname << endl;
 }
