003eb3234792f00851af2b291d118f3bef76ddb9
aquajerry <huopopo_zeyang@163.com>
2018-11-02 22:46:57 +0800

    test: implicit assignment: int division

---
+++ lesson-03-arithmetic/rational_number.cpp
@@ -0,0 +1,14 @@
+/* Practice implicit assignment: int division.
+ *
+ * What's the output of this program?
+ */
+
+#include <iostream>
+
+int main()
+{
+	int denominator = 5, numerator = 4;
+
+	int answer = numerator / denominator;
+	std::cout << "answer = " << answer;
+}
