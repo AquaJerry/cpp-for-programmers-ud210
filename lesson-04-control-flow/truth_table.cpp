e73dba944998e2d9a9fa0d8344f0a8f86fb0c56e
aquajerry <huopopo_zeyang@163.com>
2018-11-03 20:45:02 +0800

    feat: implementation: find all possible outputs for the given expression

--- lesson-04-control-flow/truth_table.cpp
+++ lesson-04-control-flow/truth_table.cpp
@@ -10,6 +10,19 @@
 
 int main()
 {
-	// Use this output format.
-	// std::cout<<"A\tB\tC\t(A && B && C)\t\t(!B||!C)\t\t(A&&(!B||!C))\t\tQ\n";
+	int a, b, c, d, e, f, q;
+
+	std::cout << "A\tB\tC\t\t(A&&B&&C)\t(!B||!C)\t(A&&(!B||!C))\tQ\n";
+	for (size_t i = 0; i < 8; ++i)
+	{
+		a = (i bitand 4) >> 2;
+		b = (i bitand 2) >> 1;
+		c = i bitand 1;
+		d = a and b and c;
+		e = not b or not c;
+		f = a and e;
+		q = d or f;
+		std::cout << a << '\t' << b << '\t' << c << "\t\t" << d
+			<< "\t\t" << e << "\t\t" << f << "\t\t" << q << '\n';
+	}
 }
