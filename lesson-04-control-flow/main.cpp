c57d21e3941c24a470953b204ec37804de414f20
aquajerry <huopopo_zeyang@163.com>
2018-11-03 21:33:32 +0800

    feat: design: recommend a pet by asking questions

--- lesson-04-control-flow/main.cpp
+++ lesson-04-control-flow/main.cpp
@@ -1,33 +1,35 @@
-/* Learn if-else statements in C++.
+/* Practice if-else statements in C++.
  *
- * The user guess is in the input.txt file. Change the value to test each of
- * the possible code branches.
+ * Ask questions and select a pet for the user. The questions are "Do you want
+ * a pet with fur(f), feathers(t), or scales(s)?" and "You want a pet with
+ * scales. Do you want the pet to live in water(w), on land(l), or both(b)?".
+ * The pet may be: dog, bird, fish, gecko, frog.
  */
 
 #include <iostream>
-
-#define TARGET 33
 
 using namespace std;
 
 int main()
 {
-	int guess;
+	char skin, location;
 
-	cout << "Guess a number between 0 and 100.\n";
-	cin >> guess;
-	cout << "You guessed: " << guess << ".\n";
+	cout << "Would you like an animal with fur(f), feathers(t), or scales(s)?";
+	cin >> skin;
+	cout << skin << '\n';
 
-	if (guess < TARGET)
-	{
-		cout << "Your guess is too low.\n";
-	}
-	else if (guess > TARGET)
-	{
-		cout << "Your guess is too high.\n";
-	}
-	else
-	{
-		cout << "Yay! You guessed correctly.\n";
-	}
+	// Use if-else statements to control program flow
+
+	cout << "Get a dog" << '\n';
+	cout << "Get a bird" << '\n';
+
+	cout << "Would you like an animal that lives in water(w), land(l), or both(b)?";
+	cin >> location;
+	cout << location << '\n';
+
+	// Use if-else statements to control program flow
+
+	cout << "Get a fish" << '\n';
+	cout << "Get a gecko" << '\n';
+	cout << "Get a frog" << '\n';
 }
