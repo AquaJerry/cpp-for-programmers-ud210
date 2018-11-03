c61cd113f333744e83070ec464e8d6eb3b337d65
aquajerry <huopopo_zeyang@163.com>
2018-11-03 23:22:06 +0800

    feat: query holiday package with fallthrough switch statement

--- lesson-04-control-flow/main.cpp
+++ lesson-04-control-flow/main.cpp
@@ -1,9 +1,7 @@
-/* Practice if-else statements in C++.
+/* Understand the switch statement in C++.
  *
- * Ask questions and select a pet for the user. The questions are "Do you want
- * a pet with fur(f), feathers(t), or scales(s)?" and "You want a pet with
- * scales. Do you want the pet to live in water(w), on land(l), or both(b)?".
- * The pet may be: dog, bird, fish, gecko, frog.
+ * This example does not use a break statement between the possibilities, which
+ * means all menu items below the selected one are executed.
  */
 
 #include <iostream>
@@ -12,39 +10,26 @@ using namespace std;
 
 int main()
 {
-	char skin, location;
+	char menu_item;
 
-	cout << "Would you like an animal with fur(f), feathers(t), or scales(s)?";
-	cin >> skin;
-	cout << skin << '\n';
+	cout << "Choose your holiday package:\nL: luxury package\n"
+		<< "S: standard package\nB: basic package ";
+	cin >> menu_item;
+	cout << "\nThe " << menu_item << " package includes:\n";
 
-	// Use if-else statements to control program flow
-	if ('f' == skin)
+	switch (menu_item)
 	{
-		cout << "Get a dog" << '\n';
-	}
-	else if ('t' == skin)
-	{
-		cout << "Get a bird" << '\n';
-	}
-	else
-	{
-		cout << "Would you like an animal that lives in water(w), land(l), or both(b)?";
-		cin >> location;
-		cout << location << '\n';
-
-		// Use if-else statements to control program flow
-		if ('w' == location)
-		{
-			cout << "Get a fish" << '\n';
-		}
-		else if ('l' == location)
-		{
-			cout << "Get a gecko" << '\n';
-		}
-		else
-		{
-			cout << "Get a frog" << '\n';
-		}
+		case 'L':
+			cout << "\tSpa Day\n";
+			cout << "\tSailboat Tour\n";
+		case 'S':
+			cout << "\tCity Tour\n";
+			cout << "\tComplimentary Happy Hour\n";
+		case 'B':
+			cout << "\tAirport Transfers\n";
+			cout << "\tComplimentary Breakfast\n";
+			break;
+		default:
+			cout << "Please select the L,S,B package.\n";
 	}
 }
