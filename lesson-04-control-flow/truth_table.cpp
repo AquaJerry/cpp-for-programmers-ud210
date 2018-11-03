ef7753004176d16226ec86ef6b1246aa08a89b95
aquajerry <huopopo_zeyang@163.com>
2018-11-03 19:44:08 +0800

    feat: design: find all possible outputs for the given expression

---
+++ lesson-04-control-flow/truth_table.cpp
@@ -0,0 +1,15 @@
+/* Find all possible outputs for the given expression.
+ *
+ * Hint: there are three inputs, so there are 8 possible input combinations
+ * (2^3) which means there is an output for each combination of inputs.
+ *
+ * The expression: (A and B and C) or (A and ((not B) or (not C))) = Q.
+ */
+
+#include <iostream>
+
+int main()
+{
+	// Use this output format.
+	// std::cout<<"A\tB\tC\t(A && B && C)\t\t(!B||!C)\t\t(A&&(!B||!C))\t\tQ\n";
+}
