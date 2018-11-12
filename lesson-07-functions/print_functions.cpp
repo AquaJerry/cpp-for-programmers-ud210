7dfb6cb8489c3a1995fa32d9443cc6fa3ebef7dd
aquajerry <huopopo_zeyang@163.com>
2018-11-12 22:50:39 +0800

    feat: a function printing "Functions"

---
+++ lesson-07-functions/print_functions.cpp
@@ -0,0 +1,16 @@
+/* Write a function called "print_message".
+ *
+ * The function prints "Functions".
+ */
+
+#include <iostream>
+
+void print_message()
+{
+	std::cout << "Functions";
+}
+
+int main()
+{
+	print_message();
+}
